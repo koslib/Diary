f0d3dbf9e7595a298b76d86cdea3b7f6c710982c
Kostas Livieratos <koslibpro@gmail.com>
2015-09-23 16:51:30 +0300

    Initial Commit

---
+++ Diary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Diary
+//
+//  Created by Konstantinos Livieratos on 9/23/15.
+//  Copyright © 2015 Konstantinos Livieratos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
